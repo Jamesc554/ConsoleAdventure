b172ec4a8b30cc76fe66ef0b12f00d384ac81c57
GammaS <jamesc554544@gmail.com>
2016-01-20 18:27:12 +0000

    Revert "Revert "First Commit""
    This reverts commit 1c60ed113160b529d5006dcceea457cdb1ad7319.

---
+++ TextBasedAdventure/Monster.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedAdventure
+{
+    public class Monster : ICloneable
+    {
+        public int health, maxHealth, level, attack, defence, baseHealth, baseAttack, baseDefence;
+        public double healthMultiplier, attackMultiplier, defenceMultiplier;
+        public string name;
+        public Location location;
+
+        public Monster(string name, Location location)
+        {
+            this.name = name;
+            this.location = location;
+        }
+
+        public void calculateStats()
+        {
+            maxHealth = (int)(baseHealth + (level  * healthMultiplier));
+            attack = (int)(baseAttack + (level * attackMultiplier));
+            defence = (int)(baseDefence + (level * defenceMultiplier));
+
+            health = maxHealth;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ TextBasedAdventure/Player.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedAdventure
+{
+    public class Player : ICloneable
+    {
+        public int health, maxHealth, level, attack, defence, baseHealth, baseAttack, baseDefence;
+        public double healthMultiplier, attackMultiplier, defenceMultiplier;
+        public string name;
+        public Location location;
+
+        public Player()
+        {
+
+        }
+
+        public void calculateStats()
+        {
+            maxHealth = (int)(baseHealth + (level  * healthMultiplier));
+            attack = (int)(baseAttack + (level * attackMultiplier));
+            defence = (int)(baseDefence + (level * defenceMultiplier));
+
+            health = maxHealth;
+        }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public void Goto(Location location)
+        {
+            this.location = location;
+        }
+    }
+}
